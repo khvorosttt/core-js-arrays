081dcd8329fb1f1dd1caaf0bf1e11f2b33f0af48
khvorosttt <khvorosttt@yandex.by>
2023-12-28 20:46:45 +0300

    feat: add method implementation sortCitiesArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -444,8 +444,24 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sort(a, b) {
+  if (a.country > b.country) {
+    return 1;
+  }
+  if (a.country < b.country) {
+    return -1;
+  }
+  if (a.city > b.city) {
+    return 1;
+  }
+  if (a.city < b.city) {
+    return -1;
+  }
+  return 0;
+}
+
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => sort(a, b));
 }
 
 /**
