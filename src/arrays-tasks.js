fb71afd73480f77c853a51297b04c062764985f5
khvorosttt <khvorosttt@yandex.by>
2023-12-28 19:06:20 +0300

    feat: add method implementation propagateItemsByPositionIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -265,8 +265,13 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const newArr = [];
+  arr.map((value, index) => {
+    newArr.push(...Array.from({ length: index + 1 }, () => value));
+    return value;
+  });
+  return newArr;
 }
 
 /**
